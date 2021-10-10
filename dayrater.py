6de7748d0f659bd5712a75858a9d269f5287b028
urbanekawit <adrianurbanekmm@gmail.com>
2021-10-10 18:11:52 +0200

    ćw1

---
+++ dayrater.py
@@ -0,0 +1,2 @@
+rating = int(input("Oceń swój dzień od od 0 do 10: "))
+             
---
+++ nameageasker.py
@@ -0,0 +1,4 @@
+name=str(input("Jak masz na imię: "))
+age=int(input("Ile masz lat?: " ))
+
+print("Twoje imię to",name,"i masz",age,"lat")
